e967231c7c29a9aa081e509b3d228f52940afc7a
Ihor Parkhomenko <gigor911@gmail.com>
2016-12-11 15:08:28 +0200

    fixed repos issue

---
+++ cache/compiled/files/36a5ecd3c279cb1d9bfa8e982a3eff25.yaml.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/ihor/Documents/finique/user/plugins/error/error.yaml',
+    'modified' => 1478495664,
+    'data' => [
+        'enabled' => true,
+        'routes' => [
+            404 => '/error'
+        ]
+    ]
+];
---
+++ cache/compiled/files/9e2dd896fa095e119a24a2ff88839f43.yaml.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/ihor/Documents/finique/system/blueprints/config/media.yaml',
+    'modified' => 1478495664,
+    'data' => [
+        'title' => 'PLUGIN_ADMIN.MEDIA',
+        'form' => [
+            'validation' => 'loose',
+            'fields' => NULL
+        ]
+    ]
+];
